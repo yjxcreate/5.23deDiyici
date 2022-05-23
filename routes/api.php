e30443abb45e9db814f9241a6592b90c0375c9f8
orangeandme <3378335470@qq.com>
2022-05-23 18:56:09 +0800

    first commit

---
+++ routes/api.php
@@ -0,0 +1,50 @@
+<?php
+
+use App\Http\Controllers\adminController;
+use App\Http\Controllers\StutestController;
+use App\Http\Controllers\uploadController;
+use App\Http\Controllers\userController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::prefix('users')->group(function (){
+    Route::post('login',[userController::class,'login']);  //用户登录   zlc
+    Route::post('registed',[userController::class,'registed']);  //用户注册  zlc
+    Route::post('again',[userController::class,'again']);  //修改用户密码  zlc
+
+});
+Route::middleware('role:user')->group(function () {
+    Route::post('test',[adminController::class,'test']);
+});
+Route::prefix('admin')->group(function (){
+    Route::post('login',[adminController::class,'login']);  //用户登录  zlc
+    Route::post('registed',[adminController::class,'registered']);  //用户注册 zlc
+    Route::post('again',[adminController::class,'again']);  //修改用户密码  zlc
+});
+Route::middleware('jwt.refresh')->group(function () {
+
+});
+Route::post('upload',[uploadController::class,'upload']);//OSS上传(不可用)
+Route::post('load',[uploadController::class,'load']);//分片上传 zlc
+
+
+
+Route::get('single',[StutestController::class,'single']);//查看单个球类信息
+Route::get('all',[StutestController::class,'all']);//查看所有球类信息
+Route::post('lend',[StutestController::class,'lend']);//借出球类
+Route::post('returnball',[StutestController::class,'returnball']);//归还球类
